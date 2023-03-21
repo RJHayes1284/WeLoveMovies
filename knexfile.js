ab98533d4be6ad562b805a9677bd18deb019f5bc
Rebekah Jeane Hayes <rebekahjhayes@gmail.com>
2023-03-20 23:09:06 -0500

    reverted database url

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgrespostgres://llsotksq:nh_FA1R_HhsL-d20bSQJPrDNlA4cEhSG@hansken.db.elephantsql.com/llsotksq ",
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
 
 module.exports = {
