b5691bfc304b450dd8cbf23eaf561b2a745a1800
Rebekah Jeane Hayes <rebekahjhayes@gmail.com>
2023-03-20 23:04:01 -0500

    updated controllers/routers/services

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,11 +1,36 @@
 const reviewsService = require("./reviews.service")
 
+async function validateReview(req, res, next) {
+    const { reviewId } = req.params;
+    const review = await reviewsService.read(reviewId);
 
+    if(review){
+        res.locals.review = review;
+        return next();
+    }
+    next({
+        status:404, 
+        message:`cannot be found: ${reviewId}`
+    });
+}
 
+async function update(req, res) {
 
+    const updatedReview = {
+      ...req.body.data,
+      review_id: res.locals.review.review_id,
+    };
 
+     await reviewsService.update(updatedReview);
 
+     res.json({ data: await reviewsService.read(updatedReview.review_id) });
+}
 
+async function destroy(req, res, next){
+    reviewsService.delete(res.locals.review.review_id)
+        .then(() => res.sendStatus(204))
+        .catch(next);
+}
 
 module.exports = {
     update: [validateReview, update],
