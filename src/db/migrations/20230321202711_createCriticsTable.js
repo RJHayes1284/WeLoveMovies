6e0fa9fd6b09cc08c719776811b327f0ed378c7b
Rebekah Jeane Hayes <rebekahjhayes@gmail.com>
2023-03-21 20:50:51 -0500

    migrations corrected

--- src/db/migrations/20230321202711_createCriticsTable.js
+++ src/db/migrations/20230321202711_createCriticsTable.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex) {
-  return knex.schema.creatTable("critics", (table) => {
+  return knex.schema.createTable("critics", (table) => {
     table.increments("critic_id").primary()
     table.string("preferred_name")
     table.string("surname")
